df81ff229b5c19e3817337bd29d7fb30d0162002
Jussilea <jussileagf@gmail.com>
2025-12-17 14:13:30 -0300

    Meu primeiro commit

---
+++ primeiroPrograma.py
@@ -0,0 +1,18 @@
+print("Hello World!")
+
+# Variaveis
+idade = 29
+nome = "Jussilea"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5,6]
+
+# Metodos
+lista_invertida = lista.reverse
